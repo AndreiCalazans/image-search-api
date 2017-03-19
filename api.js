f5da62fa9cdf42d64381ff7a3f48295d136f167d
AndreiCalazans <andreixoc@hotmail.com>
2017-03-19 11:36:02 -0300

    finished

--- api.js
+++ api.js
@@ -18,8 +18,9 @@ return newData;
 
 
 module.exports = {
-  getImage: function(term) {
-    var url = 'https://www.googleapis.com/customsearch/v1?q='+term+'&key='+keys.key+'&cx='+keys.cx+'&num=10&searchType=image';
+  getImage: function(term , index) {
+
+    var url = 'https://www.googleapis.com/customsearch/v1?q='+term+'&key='+keys.key+'&cx='+keys.cx+'&num=10&start='+index+'&searchType=image';
     return axios.get(url).then(function (res) {
       let result = imageReducer(res.data.items);
       return result;
@@ -29,14 +30,7 @@ module.exports = {
   }
 };
 
-//
-// 1 - when /nameofImage
-// the fetch must return this
-// image url
-// alt text
-// page url
-// 2 - ?offset=2
-// this will make the api skip pages
-//
+
+
 // 3 - get a list of most recent submitted searches
 // is this list it will have when and the term
--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var express =  require('express');
 const app = express();
 const path = require('path');
-
+const mongoose = require('mongoose');
 const api = require('./api');
+const favicon = require('serve-favicon');
+
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
+mongoose.connect('mongodb://andrei:123456@ds041526.mlab.com:41526/image-search-terms')
+
+const termSchema = new mongoose.Schema({
+  term: String,
+  createdAt: String
+});
+
+const Terms = mongoose.model('Terms', termSchema);
 
 
 const PORT  = process.env.PORT || 3000;
@@ -20,14 +32,36 @@ app.use(function(req, res, next){
 app.get('/' , function(req, res, next) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
+app.get('/history', function(req, res, next) {
+  Terms.find({},{_id: 0, term:1, createdAt: 1} ,function(err , terms) {
+    if (err) {
+      return console.log(err);
+    }
+    terms.sort(function (a , b ) {
+      return new Date(b.createdAt) - new Date(a.createdAt);
+    });
 
-app.get('/:term', function(req, res, next) {
-  api.getImage(req.params.term).then(function(data) {
+    res.send(terms);
+  })
+});
+
+app.get('/search/:term', function(req, res, next) {
+  Terms(parseTerm(req.params.term)).save(); // save to database the term
+  let index = Number(req.query.offset) > 1 ? Number(req.query.offset) *10 : 1;
+  api.getImage(req.params.term , index).then(function(data) {
     res.send(data);
   });
-  
+
 });
 
 app.listen(PORT, function() {
   console.log('server is up on port ' + PORT);
 });
+
+
+function parseTerm (term) {
+  return {
+    term: term,
+    createdAt: new Date()
+  }
+};
